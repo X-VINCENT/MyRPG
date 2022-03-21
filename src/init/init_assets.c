64e0fd6445dd7edf20a6db48e853ba4ca7337718
X-VINCENT <xavier.vincent@epitech.eu>
2022-03-21 17:07:55 +0100

    [EDIT] Cursor Rect

--- src/init/init_assets.c
+++ src/init/init_assets.c
@@ -18,9 +18,9 @@ void init_assets(game_t *game)
 void init_cursor(game_t *game)
 {
     const char path[] = "assets/pictures/icons/cursor_icon.png";
-    sfIntRect rect = init_rect(0, 0, 326, 326);
+    sfIntRect rect = init_rect(75, 23, 300, 326);
     sfVector2f pos = init_pos(10, 10);
-    sfVector2f scale = {0.50, 0.50};
+    sfVector2f scale = {0.25, 0.25};
 
     game->assets->cursor = create_sprite(path, rect, pos, scale);
 }
