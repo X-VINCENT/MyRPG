22e44c41ea67ee6d8ce9a92a08caf927feb4471d
X-VINCENT <xavier.vincent@epitech.eu>
2022-03-21 16:55:10 +0100

    [CLEAN] Project

--- src/main.c
+++ src/main.c
@@ -7,14 +7,14 @@
 
 #include "rpg.h"
 
-int main(int argc, char **argv, char **arge)
+int main(const int argc, const char **argv, char **arge)
 {
     if (env(arge) == ERROR)
         return ERROR;
     if (argc == 2 && !(my_strcmp(argv[1], "-h")))
         return print_help();
     if (argc != 1)
-        return my_puterror("Invalid Args\n\nCheck: ./my_defender -h");
+        return my_puterror("Invalid Args\n\nCheck: ./my_rpg -h");
     if (check_assets() == ERROR)
         return ERROR;
     return my_rpg();
