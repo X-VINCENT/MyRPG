797a4ea9befc4dfddbf11768ae12a305434f3840
X-VINCENT <xavier.vincent@epitech.eu>
2022-03-21 17:09:15 +0100

    [EDIT] Cursor Rect && ZIP Assets

--- src/init/init_assets.c
+++ src/init/init_assets.c
@@ -19,7 +19,7 @@ void init_cursor(game_t *game)
 {
     const char path[] = "assets/pictures/icons/cursor_icon.png";
     sfIntRect rect = init_rect(75, 23, 300, 326);
-    sfVector2f pos = init_pos(10, 10);
+    sfVector2f pos = init_pos(0, 0);
     sfVector2f scale = {0.25, 0.25};
 
     game->assets->cursor = create_sprite(path, rect, pos, scale);
