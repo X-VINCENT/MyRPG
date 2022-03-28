8f6ae4a466cd924218517d226d77d846c80e69a4
MaxPeix <max.peixoto@epitech.eu>
2022-03-28 14:52:44 +0200

    [FIX] destroy home menu (xavier regarde pas tu vas pleurer)

--- src/destroy/assets/home_menu/destroy.c
+++ src/destroy/assets/home_menu/destroy.c
@@ -11,8 +11,12 @@ void destroy_home_menu(home_menu_t *menu)
 {
     destroy_parallax(menu->parallax);
     sfSprite_destroy(menu->quit[0]);
+    sfSprite_destroy(menu->quit[1]);
+    sfSprite_destroy(menu->quit[2]);
     free(menu->quit);
     sfSprite_destroy(menu->options[0]);
+    sfSprite_destroy(menu->options[1]);
+    sfSprite_destroy(menu->options[2]);
     free(menu->options);
     free(menu);
 }
