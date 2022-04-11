c4005f983e185097b60e703808906ab59b1db1ef
Hugo Dubois <duboish00@gmail.com>
2022-04-11 12:21:59 +0200

    [FIX] enter appartment

--- src/event/rat/move_rat_check.c
+++ src/event/rat/move_rat_check.c
@@ -11,7 +11,7 @@ void check_rat_key_pressed(game_t *game)
 {
     sfEvent *event = game->event->event;
 
-    if (time_elapsed(game->assets->rat->movement_clock) > 0.01) {
+    if (time_elapsed(game->assets->rat->movement_clock) > 0.02) {
         move_rat(game);
         sfClock_restart(game->assets->rat->movement_clock);
     }
