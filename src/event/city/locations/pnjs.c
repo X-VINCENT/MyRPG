7ec860872781ca2d102364959d687cea24a15575
Gautier Bonhur <bonhur.gautier@gmail.com>
2022-04-12 12:41:45 +0200

    [DISPLAY] pnj and text

--- src/event/city/locations/pnjs.c
+++ src/event/city/locations/pnjs.c
@@ -7,19 +7,36 @@
 
 #include "rpg.h"
 
+void change_bool_pnj_text(pnj_t *pnj)
+{
+    if (sfTime_asSeconds(
+        sfClock_getElapsedTime(pnj->timer_display_text))
+        > pnj->time_between_text) {
+        pnj->text_index_display += 1;
+        sfClock_restart(pnj->timer_display_text);
+    }
+    if (!pnj->text_to_display[
+        pnj->text_index_display])
+        pnj->text_index_display = 0;
+}
+
 bool check_pnj_intersects(game_t *game)
 {
     sfFloatRect pnj;
     sfFloatRect rat = sfSprite_getGlobalBounds(game->assets->rat->idle_front);
 
     for (int i = 0; game->assets->pnj[i] != NULL; i++) {
+        change_bool_pnj_text(game->assets->pnj[i]);
         pnj = sfSprite_getGlobalBounds(game->assets->pnj[i]->sprite);
         if (sfFloatRect_intersects(&pnj, &rat, NULL) == sfTrue) {
             game->assets->pnj[i]->display_the_text = true;
             return true;
         } else {
             game->assets->pnj[i]->display_the_text = false;
+            sfClock_restart(game->assets->pnj[i]->timer_display_text);
+            game->assets->pnj[i]->text_index_display = 0;
             return false;
         }
     }
+    return false;
 }
--- src/init/assets/pnj/init_pnj.c
+++ src/init/assets/pnj/init_pnj.c
@@ -13,15 +13,20 @@ void init_black_pnj(pnj_t *pnj)
 
     pnj->sprite = create_sprite(PNJ_BLACK_PATH, (sfIntRect){0, 30, 45, 65},
         (sfVector2f){2324, 1690}, (sfVector2f){0.5, 0.5});
+    set_sprite_origin(pnj->sprite, (sfIntRect){0, 30, 45, 65});
     pnj->text_to_display = malloc(sizeof(sfText *) * nbr_text);
     pnj->text_to_display[nbr_text] = NULL;
     pnj->text_to_display[0] = create_text(FONT_BUENARD, sfBlack,
         10, "Hello, I'm Jacques\n");
     pnj->text_to_display[1] = create_text(FONT_BUENARD, sfBlack,
         10, "I'm the blacksmith of the city\n");
+    set_text_origin(pnj->text_to_display[0]);
+    set_text_origin(pnj->text_to_display[1]);
+    sfText_setPosition(pnj->text_to_display[0], (sfVector2f){2324, 1670});
+    sfText_setPosition(pnj->text_to_display[1], (sfVector2f){2324, 1670});
     sfText_setPosition(pnj->text_to_display[0], (sfVector2f){2324, 1680});
     sfText_setPosition(pnj->text_to_display[1], (sfVector2f){2324, 1680});
-    pnj->time_between_text = 5;
+    pnj->time_between_text = 2;
     pnj->display_the_text = false;
     pnj->text_index_display = 0;
     pnj->timer_display_text = sfClock_create();
