d68d45aaca2afd80453cfc45c9cbe9deb21357c4
gbonhur <bonhur.gautier@gmail.com>
2022-04-13 13:19:27 +0200

    [FIX] pnjs segv

--- src/init/assets/pnj/init_pnj.c
+++ src/init/assets/pnj/init_pnj.c
@@ -35,9 +35,10 @@ void init_text_pnj(pnj_t *pnj, sfVector2f pos, char *first_message)
     }
 }
 
-void init_black_pnj(pnj_t *pnj, sfVector2f pos, char *first_message, bool text)
+void init_black_pnj(pnj_t *pnj, sfVector2f pos, char *first_message, bool text,
+                        game_t *game)
 {
-    pnj->sprite = create_sprite(PNJ_BLACK_PATH, (sfIntRect){0, 30, 45, 65},
+    pnj->sprite = create_sprite(game->textures->black_pnj, (sfIntRect){0, 30, 45, 65},
         pos, (sfVector2f){0.5, 0.5});
     set_sprite_origin(pnj->sprite, (sfIntRect){0, 30, 45, 65});
     init_text_pnj(pnj, pos, first_message);
@@ -56,7 +57,7 @@ void citizens_png(game_t *game)
     for (int i = PNJ_BLACK_THREE + 1; i < 500; i++) {
         default_pnj_position.x = 1500 + rand() % (2900 + 1 - 1500);;
         default_pnj_position.y = 1700 + rand() % (1950 + 1 - 1700);;
-        init_black_pnj(assets->pnj[i], default_pnj_position, "", false);
+        init_black_pnj(assets->pnj[i], default_pnj_position, "", false, game);
     }
 }
 
@@ -71,10 +72,10 @@ void init_struct_pnjs(game_t *game)
         assets->pnj[i] = malloc(sizeof(pnj_t));
     assets->pnj[PNJ_NBR] = NULL;
     init_black_pnj(assets->pnj[PNJ_BLACK], (sfVector2f){2324, 1800},
-        "Hello, I'm Jacques", true);
+        "Hello, I'm Jacques", true, game);
     init_black_pnj(assets->pnj[PNJ_BLACK_TWO], (sfVector2f){2280, 1750},
-        "Hello, I'm Harry the brother of Jacques", true);
+        "Hello, I'm Harry the brother of Jacques", true, game);
     init_black_pnj(assets->pnj[PNJ_BLACK_THREE], (sfVector2f){2400, 1700},
-        "Hello, I'm Harry the dad of Harry !", true);
+        "Hello, I'm Harry the dad of Harry !", true, game);
     citizens_png(game);
 }
