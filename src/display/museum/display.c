7bc783ff48e8fc98f97601b4a5de5ae3aba456f6
X-VINCENT <xavier.vincent@epitech.eu>
2022-04-28 14:26:06 +0200

    [ADD] Museum Views and Rat Speed

--- src/display/museum/display.c
+++ src/display/museum/display.c
@@ -7,11 +7,34 @@
 
 #include "rpg.h"
 
+void set_view_museum(game_t *game)
+{
+    sfVector2f center;
+    sfVector2f rat_pos = sfSprite_getPosition(game->assets->rat->idle_front);
+    sfFloatRect r_museum = {0, 0, 512, 683};
+    sfFloatRect r_museum1 = {421, 1091, 545, 673};
+    sfFloatRect r_museum2 = {2552, 417, 448, 619};
+    sfFloatRect r_museum3 = {2017, 1383, 639, 544};
+    sfFloatRect r_museum4 = {0, 2127, 512, 299};
+
+    if (sfFloatRect_contains(&r_museum, rat_pos.x, rat_pos.y))
+        center = VIEW_MUSEUM_POS;
+    if (sfFloatRect_contains(&r_museum1, rat_pos.x, rat_pos.y))
+        center = VIEW_MUSEUM_POS_1;
+    if (sfFloatRect_contains(&r_museum2, rat_pos.x, rat_pos.y))
+        center = VIEW_MUSEUM_POS_2;
+    if (sfFloatRect_contains(&r_museum3, rat_pos.x, rat_pos.y))
+        center = VIEW_MUSEUM_POS_3;
+    if (sfFloatRect_contains(&r_museum4, rat_pos.x, rat_pos.y))
+        center = VIEW_MUSEUM_POS_4;
+    sfView_setCenter(game->view, center);
+    sfView_setSize(game->view, VIEW_MUSEUM_SIZE);
+}
+
 void display_museum(game_t *game)
 {
     museum_t *museum = game->assets->museum;
 
-    sfView_setCenter(game->view, VIEW_MUSEUM_POS);
-    sfView_setSize(game->view, VIEW_MUSEUM_SIZE);
+    set_view_museum(game);
     sfRenderWindow_drawSprite(game->window, museum->bg, NULL);
 }
