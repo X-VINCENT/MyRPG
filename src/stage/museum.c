88ee684191d37da177965fd1279407ceaff19c28
X-VINCENT <xavier.vincent@epitech.eu>
2022-04-29 12:29:28 +0200

    [ADD] Musem Objects

--- src/stage/museum.c
+++ src/stage/museum.c
@@ -21,6 +21,9 @@ void museum_stage(game_t *game)
     display_rat(game);
     sfRenderWindow_drawSprite(game->window,
         game->assets->museum->bg_top, NULL);
+    display_objects(game->window, game->assets->museum->objects);
+    event_objects(game->assets->rat->idle_front, game->inventory->items,
+        game->assets->museum->objects, game->keys[INTERACT]);
     display_circle_rat(game);
     display_inventory(game);
 }
